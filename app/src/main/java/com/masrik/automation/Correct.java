aa97864df54b0a9aa8aadead4c8847679d6c36f2
Masrik Dahir <dahirma@vcu.edu>
2021-12-12 16:19:40 -0500

    Initial commit

---
+++ app/src/main/java/com/masrik/automation/Correct.java
@@ -0,0 +1,28 @@
+package com.masrik.automation;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dfa_game.R;
+
+public class Correct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.correct);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+
+    }
+
+}
---
+++ app/src/main/java/com/masrik/automation/Incorrect.java
@@ -0,0 +1,28 @@
+package com.masrik.automation;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dfa_game.R;
+
+public class Incorrect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.incorrect);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+
+    } // end onCreate
+
+}
